0d091349acfd1776004541eb9314e9a747c9dd50
badmoham <badmoham@users.noreply.github.com>
2023-11-23 00:22:09 +0330

    - TestOrder(test_annotate_with_total_spending) Done

--- user_management/querysets.py
+++ user_management/querysets.py
@@ -1,9 +1,9 @@
-from django.db.models import QuerySet
+from django.db.models import QuerySet, Sum
 
 
 class CustomerQuerySet(QuerySet):
     def annotate_with_total_spending(self):
-        return self
+        return self.annotate(total_spending=Sum("order__total_price"))
 
     def annotate_with_order_count(self):
         return self

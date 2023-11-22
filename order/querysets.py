6ca604243e978d4691108c29d499d2bfdf33e375
badmoham <badmoham@users.noreply.github.com>
2023-11-22 23:07:15 +0330

    filling the dock-strings

--- order/querysets.py
+++ order/querysets.py
@@ -9,16 +9,19 @@ class OrderQuerySet(QuerySet):
         return self
 
     def total_price(self):
+        """ return total price of a queryset """
         total_price = self.all().annotate(
             price_sum=Round(Sum(F('orderitem__product__price')*F('orderitem__quantity')), 2)
         ).aggregate(sum=Sum("price_sum"))["sum"]
         return total_price
 
     def total_price_by_customer(self, customer):
+        """ return total price of a queryset by a specific customer """
         total_price = self.filter(customer=customer).annotate(
             price_sum=Round(Sum(F('orderitem__product__price')*F('orderitem__quantity')), 2)
         ).aggregate(sum=Sum("price_sum"))["sum"]
         return total_price
 
     def submitted_in_date(self, date_value):
+        """ will return a filtered queryset of a queryset for a specific date """
         return self.filter(date=make_aware(datetime.strptime(date_value, '%Y-%m-%d')))

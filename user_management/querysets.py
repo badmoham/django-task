dc6552bf43c01a0de2de60afb2f21ea856a04e31
badmoham <badmoham@users.noreply.github.com>
2023-11-23 00:26:36 +0330

    - TestOrder(annotate_with_order_count) Done

--- user_management/querysets.py
+++ user_management/querysets.py
@@ -1,9 +1,11 @@
-from django.db.models import QuerySet, Sum
+from django.db.models import QuerySet, Sum, Count
 
 
 class CustomerQuerySet(QuerySet):
     def annotate_with_total_spending(self):
+        """ will return the queryset with annotated values for each customer's total spending """
         return self.annotate(total_spending=Sum("order__total_price"))
 
     def annotate_with_order_count(self):
-        return self
+        """ will return the queryset with annotated values for each customer's total order count """
+        return self.annotate(order_count=Count("order"))
